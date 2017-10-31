7a6bde464f84201297a05d9006c154b8b08b4b02
Yerim Hu <yerimhu88@gmail.com>
2017-10-31 11:39:12 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,35 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if ( age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else if (age <= 12) {
+    return "You are a kid"
+  } else if (age > 19) {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  switch(age) {
+    case age = 13, 14, 15, 16, 17, 18, 19 :
+      return "You are a teenager"
+  }
+  console.log("You have an age")
 }
